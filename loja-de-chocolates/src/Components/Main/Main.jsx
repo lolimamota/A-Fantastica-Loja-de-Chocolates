2628fcc69c7adf2ee055bcdee6fe4eee5c5cf5aa
Lorrayne <lorraynelimamota@gmail.com>
2024-02-17 16:36:27 -0300

    renamed: images modified: card and main

--- loja-de-chocolates/src/Components/Main/Main.jsx
+++ loja-de-chocolates/src/Components/Main/Main.jsx
@@ -1,14 +1,11 @@
 import React, {useState} from "react";
 import Card from './Card.jsx';
-import Barras_3 from '../../assets/3_barras.png';
-import Square_3 from '../../assets/3quadrados.png';
-import Art from '../../assets/barras-artesanais.png';
-import Pilha from '../../assets/pilha-barras.png';
+import Barra from '../../assets/barrinhas.png';
 
 export default function Main(){
     return(
         <>
-        <Card photo={Barras_3} alt={"Imagem de 3 barras de chocolate, sendo uma amargo, uma ao leite e uma branco."} price={'R$ 25,00'}/>
+        <Card/>
         </>
     )
 }
