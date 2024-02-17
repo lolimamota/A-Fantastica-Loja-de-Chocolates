e28e2e597e5e0463ebc6da8bcb7101c8c8ad4a7c
Lorrayne <lorraynelimamota@gmail.com>
2024-02-17 15:43:29 -0300

    add: new images name create: card

--- loja-de-chocolates/src/App.jsx
+++ loja-de-chocolates/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header/Header.jsx'
+import Main from './Components/Main/Main.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
     <Header/>
+    <Main/>
     </>
   )
 }
